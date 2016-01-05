baaa641f8a7c715c79e7ab204acdd7edd5a66a9e
ajtoo <ajtoo.dev@gmail.com>
2016-01-04 23:26:03 -0800

    gets strings when clipboard is updated. Multi-line strings become two clickable items for some reason....

--- CopyPasta/Form1.cs
+++ CopyPasta/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +13,18 @@ namespace CopyPasta
 {
     public partial class Form1 : Form
     {
+        public static BindingList<string> copied = new BindingList<string>();
+        private const int WM_CLIPBOARDUPDATE = 0x031D;
+
+        //pinvoke calls for add and remove clipboard listener functions
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +32,19 @@ namespace CopyPasta
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            CopyList.DataSource = Program.copied;
+            AddClipboardFormatListener(this.Handle);
+            CopyList.DataSource = copied;
+        }
+
+        [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.Demand, Name = "FullTrust")]
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_CLIPBOARDUPDATE)
+            {
+                copied.Add(Clipboard.GetText());
+            }
         }
     }
 }
